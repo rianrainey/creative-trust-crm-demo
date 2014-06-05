b2de310f0e7b3a43147406f7cf6868c37bf80496
Rian Rainey <rian@rianrainey.com>
2014-06-05 13:59:06 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreativeTrustCrmDemo::Application.config.secret_token = '4340703821f21b7e1e555197ef0bffd591ae00dd374e342fe45be14d18af7aed8cb51349bed565c6db272f30d8f1753f4ccc6551341b8d1fbe0127f3a441eee0'
